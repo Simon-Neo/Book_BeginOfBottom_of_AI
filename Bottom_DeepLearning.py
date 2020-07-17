2f40b79c7c63b2c223334d9cbe0effc0e21600d0
Simon-Neo <simonmatthew@naver.com>
2020-07-17 10:41:17 +0900

    My laptop legion Backup git

--- Bottom_DeepLearning.py
+++ Bottom_DeepLearning.py
@@ -59,80 +59,59 @@ class simpleNet:
 
         return loss
 
-import math
+def main():
+    (x_train, t_train) , (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+    iters_num = 10000
+    lerning_rate = 0.5
+
+    train_size = x_train.shape[0]
+    batch_size = 100
+    print(train_size)
+    print(batch_size)
+    print(train_size/batch_size)
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+
+    iter_per_epoch = max(train_size / batch_size ,1)
+
+    net = TwoLayerNet(input_size=x_train.shape[1], hidden_size=50, output_size=10)
+
+    for i in range(iters_num):
+        # batch
+        batch_mask = np.random.choice(train_size, batch_size)
+
+
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        # Gradient
+        grad = net.numerical_gradient(x_batch, t_batch)
+
+        for key in ('W1', 'B1', 'W2', 'B2'):
+            net.params[key] -= lerning_rate * grad[key]
+
+
+        loss = net.loss(x_batch, t_batch)
+        print(f'----------{i}-----------')
+        print(loss)
+        train_loss_list.append(loss)
+        if i % 100 == 0:
+            print(net.accuracy(x_train, t_train))
+            print(net.accuracy(x_test, t_test) )
+
+
+
+    print(train_loss_list)
+
+
 if(__name__ == '__main__'):
 
-    # (x_train, t_train) , (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-    #
-    # iters_num = 10000
-    # lerning_rate = 0.1
-    #
-    # train_size = x_train.shape[0]
-    # batch_size = 100
-    #
-    # train_loss_list = []
-    # train_acc_list = []
-    # test_acc_list = []
-    #
-    # iter_per_epoch = max(train_size / batch_size ,1)
-    #
-    # net = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
-    #
-    # for i in range(iters_num):
-    #     # batch
-    #     batch_mask = np.random.choice(train_size, batch_size)
-    #     x_batch = x_train[batch_mask]
-    #     t_batch = t_train[batch_mask]
-    #
-    #     # Gradient
-    #     grad = net.numerical_gradient(x_batch, t_batch)
-    #
-    #     for key in ('W1', 'B1', 'W2', 'B2'):
-    #         net.params[key] -= lerning_rate * grad[key]
-    #
-    #     loss = net.loss(x_batch, t_batch)
-    #     train_loss_list.append(loss)
-    #
-    # print(train_loss_list)
-    pass
-
-
-class Monster():
-    def __init__(self, name, att):
-        self.name = name
-        self.att = att
-        self.hp = 100
-
-    def get_hp(self):
-        return self.hp
-    def get_name(self):
-        return self.name
-
-    def __del__(self):
-        print("I'm Die.. ㅜㅠ")
-
-class Goblin(Monster):
-    def __init__(self, name, att):
-        Monster.__init__(self, name, att)
-
-    def __del__(self):
-        Monster.__del__(self)
+    main()
 
 
 
-x = ['Abc', 'EGD', 'ESFD']
-
-search_word = input('What are you looling for the Word? : ')
-
-
-try:
-    result = x.index(search_word)
-except ValueError as v_error:
-    print(v_error)
-else:
-    print(result)
-finally:
-    print('interllect')
 
 
 
